40661bf0fc91840925fd6fd4f71617183518b894
Yoshimasa MATSUMOTO <tosashimanto@gmail.com>
2019-01-23 23:37:59 +0900

    element-ui layout追加.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,27 +1,46 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        iot-camera-spa-vue
-      </h1>
-      <h2 class="subtitle">
-        My exquisite Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-      <div class="text-right">
-        <el-button type="primary" @click="handleClickSubmit">Test</el-button>
-      </div>
-    </div>
+  <section class="container posts-page">
+    <el-card style="flex: 1">
+      <el-container>
+        <el-header>
+          Header
+        </el-header>
+        <el-container>
+          <el-aside width="200px">
+            <el-tree :data="tree_data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+          </el-aside>
+          <el-container>
+            <el-main>
+              <el-table
+                :data="data2()"
+                stripe
+                style="width: 100%">
+                <el-table-column
+                  prop="date"
+                  label="Date"
+                  width="180">
+                </el-table-column>
+                <el-table-column
+                  prop="name"
+                  label="Name"
+                  width="180">
+                </el-table-column>
+                <el-table-column
+                  prop="address"
+                  label="Address">
+                </el-table-column>
+              </el-table>
+              <div class="text-right">
+                <el-button type="primary" @click="handleClickSubmit">Test</el-button>
+              </div>
+            </el-main>
+          </el-container>
+        </el-container>
+        <el-footer>
+          Footer
+        </el-footer>
+      </el-container>
+    </el-card>
   </section>
 </template>
 
@@ -30,6 +49,52 @@
   import {mapActions, mapGetters} from 'vuex'
 
   export default {
+    data() {
+      return {
+        tree_data: [
+          {
+            parent_id: '',
+            label: 'Level one 1',
+            value: '',
+            children: [
+              {
+                parent_id: '',
+                label: 'Level two 1-1',
+                value: '',
+              },
+              {
+                parent_id: '',
+                label: 'Level two 1-2',
+                value: '',
+              }
+            ]
+          },
+          {
+            parent_id: '',
+            label: 'Level one 2',
+            value: '',
+            children: [
+              {
+                parent_id: '',
+                label: 'Level two 2-1',
+                value: '',
+              },
+              {
+                parent_id: '',
+                label: 'Level two 2-2',
+                value: '',
+              }
+            ]
+          }
+        ],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
+      };
+    },
+
+
     async asyncData({store, route}) {
 
       await store.dispatch('images/getImageList')
@@ -44,46 +109,67 @@
       // ...mapGetters(['user'])
     },
     methods: {
-
+      data2() {
+        var t ={
+          tableData: [{
+            date: '2016-05-03',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }, {
+            date: '2016-05-02',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }, {
+            date: '2016-05-04',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }, {
+            date: '2016-05-01',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }]
+        };
+        return t.tableData.map(item => {
+          // image.created_at = moment(post.created_at).format('YYYY/MM/DD HH:mm:ss')
+          return item
+        })
+      },
       // 画像List
       async handleClickSubmit() {
-
         this.$router.push('/images/')
+      },
 
+      handleNodeClick(data) {
+        console.log(data);
       }
     },
   }
 </script>
 
 <style>
-
-  .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
     text-align: center;
+    line-height: 60px;
   }
 
-  .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 90vh;
   }
 
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
   }
 
-  .links {
-    padding-top: 15px;
+  body > .el-container {
+    margin-bottom: 40px;
   }
+
 </style>

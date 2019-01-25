4d91f28572e5bd7f50b0a8acea5a69fe616f0f29
Yoshimasa MATSUMOTO <tosashimanto@gmail.com>
2019-01-26 02:35:37 +0900

    d3 svg修正.

--- app/pages/d3_map/index.vue
+++ app/pages/d3_map/index.vue
@@ -4,7 +4,8 @@
       <svg id="circle-001" :width="width" :height="height"></svg>
       <div>r: {{ r }}</div>
       <div>
-        <el-button type="primary" @click="handleClickMap">Test</el-button>
+        <el-button type="primary" @click="handleClickUsMap">US Map</el-button>
+        <el-button type="primary" @click="handleClickJapanMap">Japan Map</el-button>
       </div>
     </div>
   </section>
@@ -12,7 +13,8 @@
 
 <script>
 
-  import {mapGetters} from 'vuex'
+  import {mapActions, mapGetters, mapMutations, mapState} from 'vuex';
+
 
   const d3 = require('d3');
   const topojson = require('topojson')
@@ -42,12 +44,9 @@
       // var svg = d3.select(this.$el);
       // var width = +svg.attr('width');
       // var height = +svg.attr('height');
-      // //
-      // console.log("d3 width=", width)
-      // console.log("d3 height=", height)
-      console.log("d3 svg=", svg)
-      // var projection = d3.geoAlbersUsa();
-      // var path = d3.geoPath().projection(projection);
+
+      var projection = d3.geoAlbersUsa();
+      var path = d3.geoPath().projection(projection);
 
       // await store.dispatch('d3_map/getMapJSON')
 
@@ -55,14 +54,14 @@
       // store.dispatch('images/getUsJson')
 
 
-      var svg = d3.select('#circle-001');
+      const svg = d3.select('#circle-001');
       this.circle = svg.append('circle')
         .attr('cx', this.width / 2)
         .attr('cy', this.height / 2)
         .attr('r', this.r)
         .style('fill', 'rgba(0, 0, 0, 0.8)')
 
-      // d3.json("us.json", function (error, us) {
+      // d3.json('http://localhost:8080/d3_map/us.json', function (error, us) {
       //   console.log("*****************************************************")
       //   console.log("error=", error)
       //   console.log("us=", us)
@@ -85,17 +84,170 @@
 
     },
 
+    async asyncData({store, route}) {
+      var projection = d3.geoAlbersUsa();
+      var path = d3.geoPath().projection(projection);
+      const svg = d3.select('#circle-001');
+
+      const japan = await store.dispatch('d3_map/getTestJSON')
+      console.log("tttttttttttttt")
+      console.log("japan", japan)
+
+
+      var g = svg.append('g');
+      //マップ描画
+      // var map = g.selectAll("path").data(japan.features)
+      //    .enter()
+      //   .append("path")
+      //   .attr("d", path)
+      //   .style("stroke", "#ffffff")
+      //   .style("stroke-width", 0.1)
+      //   .style("fill", "#5EAFC6");
+
+      // const us =  await store.dispatch('d3_map/getTestJSON')
+      // const t = {
+      //   abc:"123"
+      // }
+      // console.log("------", us)
+      // d3.json('http://localhost:8080/d3_map/us.json', function (error, us) {
+      //   if (error) throw error;
+      //   console.log("*****************************************************")
+      //   console.log("error=", error)
+      //   console.log("us=", us)
+      //   var g = svg.append('g');
+      //   g.selectAll('.state')
+      //     .data(topojson.feature(us, us.objects.usStates).features)
+      //     .enter()
+      //     .append("path")
+      //     .attr("class", "state")
+      //     .attr("d", path)
+      //     .on('mouseover', function (d) {
+      //       v.$emit('stateSelected', d.properties.STATE_ABBR)
+      //     })
+      //     .on('mouseout', function (d) {
+      //       v.$emit('stateDeselected', d.properties.STATE_ABBR)
+      //     })
+      //   g.attr('transform', 'scale(0.57)')
+      // });
+
+    },
+
     methods: {
-      handleClickMap() {
+      handleClickUsMap() {
         console.log("handleClickMap")
 
-        var svg = d3.select('#circle-001');
+        var projection = d3.geoAlbersUsa();
+        var path = d3.geoPath().projection(projection);
+        const svg = d3.select('#circle-001');
         svg.append('circle')
           .attr('cx', 80)
           .attr('cy', 100)
           .attr('r', this.r)
           .style('fill', 'rgba(255, 0, 0, 0.5)')
+
+
+        this.$axios.get('http://localhost:8080/d3_map/us.json')
+          .then(function (response) {
+
+            var us = response.data
+
+            var g = svg.append('g');
+            g.selectAll('.state')
+              .data(topojson.feature(us, us.objects.usStates).features)
+              .enter()
+              .append("path")
+              .attr("class", "state")
+              .attr("d", path)
+              .on('mouseover', function (d) {
+                v.$emit('stateSelected', d.properties.STATE_ABBR)
+              })
+              .on('mouseout', function (d) {
+                v.$emit('stateDeselected', d.properties.STATE_ABBR)
+              })
+            g.attr('transform', 'scale(0.57)')
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       },
+
+      handleClickJapanMap() {
+        console.log("handleClickJapanMap")
+
+        // var projection = d3.geoAlbersUsa();
+        // var path = d3.geoPath().projection(projection);
+        const svg = d3.select('#circle-001');
+        svg.append('circle')
+          .attr('cx', 80)
+          .attr('cy', 100)
+          .attr('r', this.r)
+          .style('fill', 'rgba(255, 0, 0, 0.5)')
+
+
+        this.$axios.get('http://localhost:8080/d3_map/japan.json')
+          .then(function (response) {
+
+            var width = 600,
+              height = 600;
+            var scale = 1600;
+
+            // const japan = this.getTestJSON()
+            var japan = response.data
+            var aProjection = d3.geoMercator()
+              .center([136.0, 35.6])
+              .translate([width / 2, height / 2])
+              .scale(scale);
+            var geoPath = d3.geoPath().projection(aProjection);
+            // var svg = d3.select("svg").attr("width", width).attr("height", height);
+
+            //マップ描画
+            var map = svg.selectAll("path").data(japan.features)
+              .enter()
+              .append("path")
+              .attr("d", geoPath)
+              .style("stroke", "#ffffff")
+              .style("stroke-width", 0.1)
+              .style("fill", "#5EAFC6");
+
+            //ズームイベント設定
+            var zoom = d3.zoom().on('zoom', function () {
+              aProjection.scale(scale * d3.event.transform.k);
+              map.attr('d', geoPath);
+            });
+            svg.call(zoom);
+
+            //ドラッグイベント設定
+            var drag = d3.drag().on('drag', function () {
+              var tl = aProjection.translate();
+              aProjection.translate([tl[0] + d3.event.dx, tl[1] + d3.event.dy]);
+              map.attr('d', geoPath);
+            });
+            map.call(drag);
+          });
+      },
+
+      test(us) {
+        var projection = d3.geoAlbersUsa();
+        var path = d3.geoPath().projection(projection);
+        const svg = d3.select('#circle-001');
+        var g = svg.append('g');
+        g.selectAll('.state')
+          .data(topojson.feature(us, us.objects.usStates).features)
+          .enter()
+          .append("path")
+          .attr("class", "state")
+          .attr("d", path)
+          .on('mouseover', function (d) {
+            v.$emit('stateSelected', d.properties.STATE_ABBR)
+          })
+          .on('mouseout', function (d) {
+            v.$emit('stateDeselected', d.properties.STATE_ABBR)
+          })
+        g.attr('transform', 'scale(0.57)')
+      },
+
+      ...mapActions('d3_map', ['createMap', 'getTestJSON']),
+      ...mapActions('images', ['getUsJson'])
     }
   }
 </script>
--- app/store/d3_map.js
+++ app/store/d3_map.js
@@ -1,5 +1,9 @@
 export const state = () => ({
-  map_json: {}
+  map_json: {},
+  d3_map :{
+    width: null,
+    height: null
+  }
 })
 
 export const getters = {
@@ -8,10 +12,16 @@ export const getters = {
 
 export const mutations = {
 
-  setMapJSON(state, { map_json }) {
+  setMapJSON(state, {map_json}) {
     console.log("setMapJSON")
     map_json = map_json
     console.log("map_json=", map_json)
+  },
+  setD3Map(state, {width, height}) {
+    console.log("setD3Map")
+    d3_map.width = width
+    d3_map.height = height
+
   },
 
 }
@@ -19,15 +29,47 @@ export const mutations = {
 export const actions = {
 
 
-  async getMapJSON({ commit }) {
+  async getMapJSON({commit}) {
 
     const map_json = await this.$axios.$get(`http://localhost:8080/d3_map/us.json`)
-    commit('setMapJSON', { map_json })
+    commit('setMapJSON', {map_json})
   },
 
-  async getTestJSON({ commit }) {
+  async getTestJSON({commit}) {
 
     const map_json = await this.$axios.$get(`http://localhost:8080/d3_map/us.json`)
     return map_json
   },
+
+  createMap({ commit }, svg, japan) {
+    var aProjection = d3.geoMercator()
+      .center([136.0, 35.6])
+      .translate([width / 2, height / 2])
+      .scale(scale);
+    var geoPath = d3.geoPath().projection(aProjection);
+
+    //マップ描画
+    var map = svg.selectAll("path").data(japan.features)
+      .enter()
+      .append("path")
+      .attr("d", geoPath)
+      .style("stroke", "#ffffff")
+      .style("stroke-width", 0.1)
+      .style("fill", "#5EAFC6");
+
+    //ズームイベント設定
+    var zoom = d3.zoom().on('zoom', function () {
+      aProjection.scale(scale * d3.event.transform.k);
+      map.attr('d', geoPath);
+    });
+    svg.call(zoom);
+
+    //ドラッグイベント設定
+    var drag = d3.drag().on('drag', function () {
+      var tl = aProjection.translate();
+      aProjection.translate([tl[0] + d3.event.dx, tl[1] + d3.event.dy]);
+      map.attr('d', geoPath);
+    });
+    map.call(drag);
+  }
 }
